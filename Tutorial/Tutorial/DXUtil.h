748d9595c25026c52b9da926e6e45f12a10be8c6
Lux_work <dodoyln@hotmail.com>
2018-08-23 12:24:25 +0300

    DirectX Error Handling

--- Tutorial/Tutorial/DXUtil.h
+++ Tutorial/Tutorial/DXUtil.h
@@ -2,8 +2,26 @@
 
 #include <d3d11.h>
 #include <DirectXColors.h>
+#include "DXERR\dxerr.h"
 
 #pragma comment(lib, "d3d11.lib")
+
+#ifdef _DEBUG
+#ifndef HR
+#define HR(x) \
+{ \
+    HRESULT hr = x;     \
+    if (FAILED(hr)) {   \
+        DXTraceW(__FILEW__, __LINE__, hr, L#x, TRUE); \
+    } \
+}
+#endif // !HR
+#else
+#ifndef HR
+#define HR(x) x;
+#endif // !HR
+#endif // _DEBUG
+
 
 namespace Memory {
      
--- Tutorial/Tutorial/winmain.cpp
+++ Tutorial/Tutorial/winmain.cpp
@@ -43,5 +43,5 @@ void TestApp::Update(float dt)
 void TestApp::Render(float dt)
 {
     m_pImmediateContext->ClearRenderTargetView(m_pRenderTargetView, DirectX::Colors::CornflowerBlue);
-    m_pSwapChain->Present(0, 0);
+    HR(m_pSwapChain->Present(0, 0));
 }
